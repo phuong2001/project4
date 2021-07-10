6503c69066ab03803f5aabf140717ca9feae94fd
Linh Nguyen <nguyenlinh3836@gmail.com>
2021-07-10 09:34:57 +0700

    Initial commit

---
+++ src/main/java/com/project/lpd/LpdApplication.java
@@ -0,0 +1,13 @@
+package com.project.lpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/lpd/LpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
