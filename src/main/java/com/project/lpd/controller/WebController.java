196f4eb15bdb6c08a6ec948e4938e3c6fc609ccd
phuong2001 <54660042+phuong2001@users.noreply.github.com>
2021-07-22 00:47:40 +0700

    list and update role

--- src/main/java/com/project/lpd/controller/WebController.java
+++ src/main/java/com/project/lpd/controller/WebController.java
@@ -1,17 +1,31 @@
 package com.project.lpd.controller;
 
+import com.project.lpd.entity.RoleEntity;
+import com.project.lpd.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.List;
 
 @Controller
 public class WebController {
+    @Autowired
+    RoleService roleService;
+
     @GetMapping({"/", "/index"})
     public String index() {
         return "index";
     }
 
     @GetMapping("/login")
-    public String loginHome(){
+    public String login(){
         return "login";
     }
 
@@ -55,4 +69,27 @@ public class WebController {
         return "help";
     }
 
+    @GetMapping({"/list"})
+    public String pageableRole(Model model, @RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "4") int size) {
+        List<RoleEntity> roles = roleService.getAllRole(PageRequest.of(page, size));
+        int totalPage  = roleService.getTotalPage(PageRequest.of(page, size));
+        model.addAttribute("totalPage", totalPage);
+        model.addAttribute("size", size);
+        model.addAttribute("page", page);
+        model.addAttribute("roles", roles);
+        return "listrole";
+    }
+
+    @GetMapping("/update")
+    public String viewUpdateRole(Model model, @RequestParam(value = "id", defaultValue = "0") int id) {
+        RoleEntity role = roleService.getRoleById(id);
+        model.addAttribute("role", role);
+        return "updaterole";
+    }
+
+    @PostMapping("/update")
+    public String updateRole(@ModelAttribute RoleEntity roleEntity, Model model) {
+        roleService.updateRole(roleEntity);
+        return "redirect:/list";
+    }
 }
--- src/main/java/com/project/lpd/entity/UserEntity.java
+++ src/main/java/com/project/lpd/entity/UserEntity.java
@@ -50,5 +50,115 @@ public class UserEntity {
     @OneToMany(mappedBy = "user")
     private List<ProductEntity> product;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(int roleid) {
+        this.roleid = roleid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public RoleEntity getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEntity role) {
+        this.role = role;
+    }
+
+    public CartEntity getCart() {
+        return cart;
+    }
+
+    public void setCart(CartEntity cart) {
+        this.cart = cart;
+    }
+
+    public CardEntity getCard() {
+        return card;
+    }
+
+    public void setCard(CardEntity card) {
+        this.card = card;
+    }
+
+    public List<ReviewEntity> getReview() {
+        return review;
+    }
+
+    public void setReview(List<ReviewEntity> review) {
+        this.review = review;
+    }
+
+    public List<TransactionEntity> getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(List<TransactionEntity> transaction) {
+        this.transaction = transaction;
+    }
+
+    public List<ProductEntity> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<ProductEntity> product) {
+        this.product = product;
+    }
 }
