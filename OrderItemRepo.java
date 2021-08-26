14e0e32d3a81fa909c8973e20fd23ca0a8be1941
Linh Nguyen <nguyenlinh3836@gmail.com>
2021-08-26 08:37:41 +0000

    Revert "Merge remote-tracking branch 'origin/tuananh0811-tuấnanh26-patch-00776'"
    This reverts commit 0fe502f2c6d017396e8a83895947dc323f68e034

--- OrderItemRepo.java
+++
@@ -1,8 +0,0 @@
-package com.project.lpd.repository;
-
-import com.project.lpd.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepo extends JpaRepository<OrderItem,Integer> {
-
-}
